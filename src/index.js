a33e8f0232e9a4c4ebb38a4031e20d36c24bd0e4
Michael <michael.s11.soares@gmail.com>
2026-08-01 12:01:48 -0300

    teste init

--- src/index.js
+++ src/index.js
@@ -9,14 +9,26 @@ import { initializeStaticActions } from "./lib/StaticAction.js";
 import { initializeStaticOptions } from "./lib/StaticOption.js";
 
 export async function init(options) {
+  console.log("INIT EXECUTADO");
+
   setConfig(options);
 
   try {
+    console.log("CRIANDO SESSÃO");
+
     const { sessionId } = await createSession();
+
+    console.log("SESSION ID:", sessionId);
 
     saveSessionId(sessionId);
 
+    console.log("SESSION SALVA");
+
+    console.log("INICIALIZANDO EVENTOS");
     initializeEvents();
+    console.log("EVENTOS INICIALIZADOS");
+
+    console.log("INICIALIZANDO RESPOSTAS");
     initializeAnswers();
 
     initializeStaticObserver();
